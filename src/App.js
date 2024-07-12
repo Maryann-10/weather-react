037d8dea76468e24992b2076dea535ff896c51ac
Maryann <maryannichaura@gmail.com>
2024-07-12 16:24:17 +0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+import Weather from './Weather';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+        </header> 
+        <Weather defaultCity="Paris" />
+        <footer>
+            This project was coded by {" "}
+            <a 
+            href="www.linkedin.com/in/maryann-njeri-4560b92a8" 
+            target="_blank" 
+            rel="noreferrer"
+            > 
+            Maryann Ichaura
+            </a>, {""}is open-sourced on {" "}
+            <a 
+            href="" target="_blank" 
+            rel="noreferrer"
+            >
+            GitHub 
+            </a>
+            {" "}and hosted on 
+            <a href=''> Netlify</a>.
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Weather.js
@@ -0,0 +1,72 @@
+import React,{useState} from "react";
+import axios from "axios";
+import "./Weather.css";
+import WeatherInfo from "./WeatherInfor";
+import WeatherForecast from "./WeatherForecast";
+
+
+export default function Weather(props) {
+    let [weatherData, setWeatherData] = useState({ ready: false });
+    let [city, setCity] = useState(props.defaultCity);
+  
+    function handleResponse(response) {
+      setWeatherData({
+        ready: true,
+        temperature: response.data.main.temp,
+        humidity: response.data.main.humidity,
+        wind: response.data.wind.speed,
+        city: response.data.name,
+        description: response.data.weather[0].description,
+        date: new Date(response.data.dt * 1000),
+        icon: response.data.weather[0].icon,
+      });
+    }
+  
+    function handleSubmit(event) {
+      event.preventDefault();
+      search();
+    }
+  
+    function handleCityChange(event) {
+      setCity(event.target.value);
+    }
+  
+   function search() {
+     let apiKey = "ad793a6d772939c31783de5822791acf";
+     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+     axios.get(apiUrl).then(handleResponse);
+   }
+  
+  
+    if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input 
+                type="search" 
+                placeholder="Enter a city ..."
+                className="form-control "
+                autoFocus="on" 
+                onChange={handleCityChange}
+                />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="Search" className="btn btn-primary w-100"/>
+            </div>
+          </div>
+        </form>
+          < WeatherInfo 
+            data={weatherData} 
+          />
+          <WeatherForecast />
+        </div>
+    );
+    } else {
+      search()
+      return "loading... "
+    }
+  }
+
+  
